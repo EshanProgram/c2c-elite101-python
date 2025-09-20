47b3ed40ea31dc03c43406008ca592da34a92c4f
EshanProgram <71158170+EshanProgram@users.noreply.github.com>
2025-09-19 21:19:55 -0500

    Update main.py
    updated the code to include sub - menu options

--- main.py
+++ main.py
@@ -19,9 +19,35 @@ def get_user_name():
 def greet_user(name):
     print(f"Hello, {name}!")
 
+def get_age():
+    return input("Please enter your age: ")
+
 def main():
     user_name = get_user_name()
     greet_user(user_name)
+    age = get_age()
+    print(f"Oh, you are {age}!")
+    
+    while True:
+        
+        print("\n Please choose from the menu for the pharmacy:")
+        print("1. Are you a medical practitioner?")
+        print("2. Are you checking status of prescription?")
+        print("3. Are you looking for refill prescription?")
+        print("4. Exit")
+        choice = input("Enter your choice:")
+        
+        if choice == "1":
+            print("Go to sub menu 1 (placeholder)")
+        elif choice == "2":  
+            print("Go to sub menu 2 (placeholder)")
+        elif choice == "3":  
+            print("Go to sub menu 3 (placeholder)")   
+        elif choice == "4":  
+            print("Have a nice rest of the day")
+            break
+        else:
+            print("Can you enter the option as in the menu")
 
 if __name__ == "__main__":
     main()
